7f12125c989688c84a81ad94e4b6b8eb14032196
Pierluigi Di Lorenzo <pi.dilorenzo@reply.de>
2017-09-25 00:48:52 +0200

    Create Master and 3 Nodes

--- instances.tf
+++ instances.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "master" {
+resource "aws_instance" "MasterInstance" {
   ami           = "${lookup(var.AmiLinux, var.region)}"
   instance_type = "t2.micro"
   associate_public_ip_address = "true"
@@ -8,6 +8,45 @@ resource "aws_instance" "master" {
   key_name = "${var.key_name}"
   tags {
         Application = "AWS::StackId"
-        Name = "Master"
+        Name = "master"
+  }
+ }
+resource "aws_instance" "Node1Instance" {
+  ami           = "${lookup(var.AmiLinux, var.region)}"
+  instance_type = "t2.micro"
+  associate_public_ip_address = "true"
+  subnet_id = "${aws_subnet.PublicAZA.id}"
+  private_ip    = "${var.node1_ip}"
+  vpc_security_group_ids = ["${aws_security_group.FrontEnd.id}"]
+  key_name = "${var.key_name}"
+  tags {
+        Application = "AWS::StackId"
+        Name = "node1"
+  }
+ }
+ resource "aws_instance" "Node2Instance" {
+  ami           = "${lookup(var.AmiLinux, var.region)}"
+  instance_type = "t2.micro"
+  associate_public_ip_address = "true"
+  subnet_id = "${aws_subnet.PublicAZA.id}"
+  private_ip    = "${var.node2_ip}"
+  vpc_security_group_ids = ["${aws_security_group.FrontEnd.id}"]
+  key_name = "${var.key_name}"
+  tags {
+        Application = "AWS::StackId"
+        Name = "node2"
+  }
+ }
+ resource "aws_instance" "Node3Instance" {
+  ami           = "${lookup(var.AmiLinux, var.region)}"
+  instance_type = "t2.micro"
+  associate_public_ip_address = "true"
+  subnet_id = "${aws_subnet.PublicAZA.id}"
+  private_ip    = "${var.node3_ip}"
+  vpc_security_group_ids = ["${aws_security_group.FrontEnd.id}"]
+  key_name = "${var.key_name}"
+  tags {
+        Application = "AWS::StackId"
+        Name = "node3"
   }
  }
